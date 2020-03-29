5d2f24cb4063c1e2e82588b4dae0e792648bd971
bekturova13 <sanirabiga@bk.ru>
2020-03-30 02:25:11 +0600

    homework1

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+public static void main(String[] args) {
+	// homework from dumbs
+        String whatIsYourName;
+        final int num = 3;
+        String word = "Kanykei";
+        whatIsYourName = num + word;
+        System.out.print(word);
+        System.out.println(whatIsYourName);
+        if(num < 0 );
+        System.out.println("Вы сохранили отрицательное число");
+        if (num>0);
+        System.out.println("Вы сохранили положительное число");
+        if (num ==0 );
+        System.out.println("Вы сохранили нуль");}
+
+    }
+
+
